83ceec17bb8e56537c93233d19a63efb31e1185b
Aliothmoon <aliothmoon@outlook.com>
2025-01-08 00:22:14 +0800

    fix: change tables prefix

--- src/main/kotlin/model/entity/entities.kt
+++ src/main/kotlin/model/entity/entities.kt
@@ -5,7 +5,7 @@ import org.ktorm.schema.datetime
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
-object CDK : Table<Nothing>("mwf_cdk") {
+object CDK : Table<Nothing>("mirrorc_cdk") {
     val key = varchar("key").primaryKey()
     val specificationId = varchar("specification_id")
     val type = varchar("type")
@@ -14,7 +14,7 @@ object CDK : Table<Nothing>("mwf_cdk") {
     val createdAt = datetime("created_at")
 }
 
-object OperationLog : Table<Nothing>("mwf_operation_log") {
+object OperationLog : Table<Nothing>("mirrorc_operation_log") {
     val id = int("id").primaryKey()
     val cdk = varchar("cdk")
     val source = varchar("source")
