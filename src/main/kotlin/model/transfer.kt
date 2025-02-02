5dedd16ac44f2cafb8e616d99ab7bee888281a5f
Aliothmoon <107878625+Aliothmoon@users.noreply.github.com>
2025-02-02 14:39:45 +0800

    feat: add buffer-trigger & validate cache (#4)

--- src/main/kotlin/model/transfer.kt
+++ src/main/kotlin/model/transfer.kt
@@ -1,9 +1,19 @@
 package model
 
+import java.time.LocalDateTime
+
 
 data class LogRecord(
     val cdk: String,
     val source: String?,
-    val spId: String,
+    val spId: String?,
     val type: String,
+    val ua: String?,
+    val time: LocalDateTime,
+)
+
+class ValidTuple(
+    val status: Int,
+    val expireTime: LocalDateTime,
+    val spId: String?,
 )
