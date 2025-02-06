ed2d8bb344892f97255dde79886cc3361a772d15
Aliothmoon <aliothmoon@outlook.com>
2025-02-06 23:11:12 +0800

    feat: add extra metrics

--- src/main/kotlin/metrics/handler.kt
+++ src/main/kotlin/metrics/handler.kt
@@ -1,8 +1,14 @@
 package metrics
 
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmHeapPressureMetrics
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
+import io.micrometer.core.instrument.binder.logging.LogbackMetrics
+import io.micrometer.core.instrument.binder.system.DiskSpaceMetrics
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
 import io.micrometer.core.instrument.binder.system.UptimeMetrics
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
@@ -16,5 +22,10 @@ fun initMetrics() {
     JvmThreadMetrics().bindTo(registry)
     JvmGcMetrics().bindTo(registry)
     JvmMemoryMetrics().bindTo(registry)
+    ProcessorMetrics().bindTo(registry)
+    FileDescriptorMetrics().bindTo(registry)
+    LogbackMetrics().bindTo(registry)
+    JvmHeapPressureMetrics().bindTo(registry)
+    ClassLoaderMetrics().bindTo(registry)
     UptimeMetrics().bindTo(registry)
 }
